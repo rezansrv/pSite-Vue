f4399f03ac574b3dccc2acb1f5713ef2a0d7b5e1
Reza Naserivand <rnaserivand@gmail.com>
2023-12-24 23:41:03 +0330

    writing page

---
+++ src/pages/writingApp.vue
@@ -0,0 +1,467 @@
+<template class="dc">
+  <div class="h">
+    <svg id="svg" style="height:0% ">
+      <text
+          x="50%"
+          y="55%"
+          font-size="150"
+          text-anchor="middle"
+          fill="white"
+          @mouseover="handleMouseOver"
+          @mouseout="handleMouseOut"
+          class="text-glitch"
+          style="z-index: 1000"
+      >
+        {{ displayText }}
+      </text>
+    </svg>
+  </div>
+</template>
+<script>
+
+export default {
+  data() {
+    return {
+
+      displayText: 'I make things', // افزودن متغیر برای نمایش متن
+    };
+  },
+
+  mounted() {
+    this.animate();
+
+
+
+  },
+  methods: {
+    //mouse hover
+    changeText(newText) {
+      this.displayText = newText;
+    },
+
+    handleMouseOver() {
+      this.mouse.power = 80;
+      this.changeText('I breake things');
+    },
+    handleMouseOut() {
+      this.mouse.power = 13;
+      this.changeText('I make things');
+    },
+
+    setStyles() {
+
+
+    },
+
+
+    init() {
+      this.screenW = window.innerWidth;
+      this.screenH = 620;
+      // Check if the screen width is less than a certain threshold (e.g., 600 pixels)
+      if (window.innerWidth < 600) {
+        this.dotNumber = 160; // Set the desired number of dots for mobile size
+      } else {
+        this.dotNumber = 207; // Set the default number of dots for larger screens
+      }
+      if (window.innerWidth < 600) {
+        this.lineNumber = 5; // Set the desired number of dots for mobile size
+      } else {
+        this.lineNumber = 6; // Set the default number of dots for larger screens
+      }
+      this.dotRows = Math.sqrt(this.dotNumber * (this.screenH / this.screenW));
+      this.dotColumns = this.dotNumber / this.dotRows;
+      var dotPosStepX = Math.round(this.screenW / this.dotColumns);
+      var dotPosStepY = Math.round(this.screenH / this.dotRows);
+      this.dotRows = Math.round(this.dotRows);
+      this.dotColumns = Math.round(this.dotColumns);
+      //     const fixedHeight = 600; // Change this value to your desired height
+      // this.svg.setAttribute('height', fixedHeight);
+      this.svg.setAttribute('height', this.screenH);
+      for (let i = 0, j = 0, k = 0; i < this.dotNumber; i++, k++) {
+        if (i % this.dotColumns === 0) {
+          j++;
+          k = 0;
+        }
+        this.dots[i] = {};
+        this.dots[i].distances = [];
+        this.dots[i].el = this.dotMatrix.cloneNode(false);
+        this.dots[i].X = k * dotPosStepX + dotPosStepX / 2;
+        this.dots[i].X += Math.floor((Math.random() * this.dotRandomMax) + 1 - this.dotRandomMax / 2);
+        this.dots[i].Y = j * dotPosStepY - dotPosStepY / 2;
+        this.dots[i].Y += Math.floor((Math.random() * this.dotRandomMax) + 1 - this.dotRandomMax / 2);
+        this.dots[i].r = 1;
+        this.dots[i].lines = [];
+        this.dots[i].el.setAttribute('cx', this.dots[i].X);
+        this.dots[i].el.setAttribute('cy', this.dots[i].Y);
+        this.dots[i].el.setAttribute('r', this.dots[i].r);
+        this.svg.appendChild(this.dots[i].el);
+        for (let l = 0; l < this.lineNumber; l++) {
+          this.dots[i].lines[l] = {};
+          this.dots[i].lines[l].el = this.lineMatrix.cloneNode(false);
+          this.dots[i].lines[l].X1 = this.dots[i].X;
+          this.dots[i].lines[l].Y1 = this.dots[i].Y;
+          this.dots[i].lines[l].X2 = this.dots[i].X;
+          this.dots[i].lines[l].Y2 = this.dots[i].Y;
+          this.dots[i].lines[l].el.setAttribute('stroke', 'rgba(0,255,255,0.05)');
+          this.dots[i].lines[l].el.setAttribute('stroke-width', '1');
+          this.svg.appendChild(this.dots[i].lines[l].el);
+        }
+      }
+    },
+  }
+};
+</script>
+
+
+<style scoped>
+.dc {
+  margin: 0;
+
+}
+.h{
+  height: 50% !important;
+}
+svg, html, body {
+  box-sizing: border-box;
+  height: 50%;
+  width: 100%;
+  padding: 0;
+  margin: 0;
+  background-image: radial-gradient(circle, rgb(20, 22, 24), #111827);
+}
+
+svg circle {
+  position: relative;
+  z-index: 100;
+}
+
+svg line {
+  position: relative;
+  z-index: 0;
+}
+
+.text-glitch {
+  text-transform: uppercase;
+  font-size: calc(18vmin + 8*(100vw - 400px)/ 480);
+  font-size:11.5vw;
+  cursor: pointer;
+  position: relative;
+  z-index: 1000;
+  text-align: center;
+  font-family: Gotham XNarrow A, Gotham XNarrow B, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica Neue, Arial, sans-serif;
+  text-transform: uppercase;
+  font-weight: bold; /* Changed from 400 to bold */
+  font-style: normal;
+  color: white;
+  animation: glitch 2s steps(100) infinite;
+}
+@keyframes glitch {
+  0% {
+    text-shadow: 1px 0 0 red, -1px 0 0 blue;
+  }
+  1% {
+    text-shadow: 2px 0 0 red, -2px 0 0 blue;
+  }
+  2% {
+    text-shadow: 3px 0 0 red, -3px 0 0 blue;
+  }
+  3% {
+    text-shadow: 4px 0 0 red, -4px 0 0 blue;
+  }
+  4% {
+    text-shadow: 5px 0 0 red, -5px 0 0 blue;
+  }
+  5% {
+    text-shadow: 4px 0 0 red, -4px 0 0 blue;
+  }
+  6% {
+    text-shadow: 3px 0 0 red, -3px 0 0 blue;
+  }
+  7% {
+    text-shadow: 2px 0 0 red, -2px 0 0 blue;
+  }
+  8% {
+    text-shadow: 1px 0 0 red, -1px 0 0 blue;
+  }
+  9% {
+    text-shadow: 0px 0 0 red, 0px 0 0 blue;
+  }
+  10% {
+    text-shadow: -1px 0 0 red, 1px 0 0 blue;
+  }
+  11% {
+    text-shadow: 0.5px 0 0 red, -0.5px 0 0 lime;
+  }
+  12% {
+    text-shadow: 1px 0 0 red, -1px 0 0 lime;
+  }
+  13% {
+    text-shadow: 1.5px 0 0 red, -1.5px 0 0 lime;
+  }
+  14% {
+    text-shadow: 2px 0 0 red, -2px 0 0 lime;
+  }
+  15% {
+    text-shadow: 2.5px 0 0 red, -2.5px 0 0 lime;
+  }
+  16% {
+    text-shadow: 1.5px 0 0 red, -1.5px 0 0 lime;
+  }
+  17% {
+    text-shadow: 0.5px 0 0 red, -0.5px 0 0 lime;
+  }
+  18% {
+    text-shadow: -0.5px 0 0 red, 0.5px 0 0 lime;
+  }
+  19% {
+    text-shadow: -1.5px 0 0 red, 1.5px 0 0 lime;
+  }
+  20% {
+    text-shadow: -2.5px 0 0 red, 2.5px 0 0 lime;
+  }
+  21% {
+    text-shadow: 0.7px 0 0 blue, -0.7px 0 0 lime;
+  }
+  22% {
+    text-shadow: 1.4px 0 0 blue, -1.4px 0 0 lime;
+  }
+  23% {
+    text-shadow: 2.1px 0 0 blue, -2.1px 0 0 lime;
+  }
+  24% {
+    text-shadow: 2.8px 0 0 blue, -2.8px 0 0 lime;
+  }
+  25% {
+    text-shadow: 3.5px 0 0 blue, -3.5px 0 0 lime;
+  }
+  26% {
+    text-shadow: 4.2px 0 0 blue, -4.2px 0 0 lime;
+  }
+  27% {
+    text-shadow: 4.9px 0 0 blue, -4.9px 0 0 lime;
+  }
+  28% {
+    text-shadow: 5.6px 0 0 blue, -5.6px 0 0 lime;
+  }
+  29% {
+    text-shadow: 6.3px 0 0 blue, -6.3px 0 0 lime;
+  }
+  30% {
+    text-shadow: 7px 0 0 blue, -7px 0 0 lime;
+  }
+  31% {
+    text-shadow: 6px 0 0 blue, -6px 0 0 lime;
+  }
+  32% {
+    text-shadow: 5px 0 0 blue, -5px 0 0 lime;
+  }
+  33% {
+    text-shadow: 4px 0 0 blue, -4px 0 0 lime;
+  }
+  34% {
+    text-shadow: 3px 0 0 blue, -3px 0 0 lime;
+  }
+  35% {
+    text-shadow: 2px 0 0 blue, -2px 0 0 lime;
+  }
+  36% {
+    text-shadow: 1px 0 0 blue, -1px 0 0 lime;
+  }
+  37% {
+    text-shadow: 0px 0 0 blue, 0px 0 0 lime;
+  }
+  38% {
+    text-shadow: -1px 0 0 blue, 1px 0 0 lime;
+  }
+  39% {
+    text-shadow: -2px 0 0 blue, 2px 0 0 lime;
+  }
+  40% {
+    text-shadow: -3px 0 0 blue, 3px 0 0 lime;
+  }
+  41% {
+    text-shadow: 50px 0 0 blue, -50px 0 0 lime;
+  }
+  42% {
+    text-shadow: 0 0 0 blue, 0 0 0 lime;
+  }
+  43% {
+    text-shadow: 0.5px 0 0 red, -0.5px 0 0 lime;
+  }
+  44% {
+    text-shadow: 1px 0 0 red, -1px 0 0 lime;
+  }
+  45% {
+    text-shadow: 1.5px 0 0 red, -1.5px 0 0 lime;
+  }
+  46% {
+    text-shadow: 2px 0 0 red, -2px 0 0 lime;
+  }
+  47% {
+    text-shadow: 1px 0 0 red, -1px 0 0 lime;
+  }
+  48% {
+    text-shadow: 0px 0 0 red, 0px 0 0 lime;
+  }
+  49% {
+    text-shadow: -1px 0 0 red, 1px 0 0 lime;
+  }
+  50% {
+    text-shadow: -2px 0 0 red, 2px 0 0 lime;
+  }
+  51% {
+    text-shadow: 1px 0 0 red, -1px 0 0 blue;
+  }
+  52% {
+    text-shadow: 2px 0 0 red, -2px 0 0 blue;
+  }
+  53% {
+    text-shadow: 3px 0 0 red, -3px 0 0 blue;
+  }
+  54% {
+    text-shadow: 4px 0 0 red, -4px 0 0 blue;
+  }
+  55% {
+    text-shadow: 5px 0 0 red, -5px 0 0 blue;
+  }
+  56% {
+    text-shadow: 4px 0 0 red, -4px 0 0 blue;
+  }
+  57% {
+    text-shadow: 3px 0 0 red, -3px 0 0 blue;
+  }
+  58% {
+    text-shadow: 2px 0 0 red, -2px 0 0 blue;
+  }
+  59% {
+    text-shadow: 1px 0 0 red, -1px 0 0 blue;
+  }
+  60% {
+    text-shadow: 0px 0 0 red, 0px 0 0 blue;
+  }
+  61% {
+    text-shadow: 30px 0 0 red, -30px 0 0 lime;
+  }
+  62% {
+    text-shadow: 0 0 0 red, 0 0 0 lime;
+  }
+  63% {
+    text-shadow: 0.5px 0 0 red, -0.5px 0 0 blue;
+  }
+  64% {
+    text-shadow: 1px 0 0 red, -1px 0 0 blue;
+  }
+  65% {
+    text-shadow: 1.5px 0 0 red, -1.5px 0 0 blue;
+  }
+  66% {
+    text-shadow: 2px 0 0 red, -2px 0 0 blue;
+  }
+  67% {
+    text-shadow: 1px 0 0 red, -1px 0 0 blue;
+  }
+  68% {
+    text-shadow: 0px 0 0 red, 0px 0 0 blue;
+  }
+  69% {
+    text-shadow: -1px 0 0 red, 1px 0 0 blue;
+  }
+  70% {
+    text-shadow: -2px 0 0 red, 2px 0 0 blue;
+  }
+  71% {
+    text-shadow: 70px 0 0 red, -70px 0 0 blue;
+  }
+  72% {
+    text-shadow: 0 0 0 red, 0 0 0 blue;
+  }
+  73% {
+    text-shadow: 1px 0 0 red, -1px 0 0 blue;
+  }
+  74% {
+    text-shadow: 2px 0 0 red, -2px 0 0 blue;
+  }
+  75% {
+    text-shadow: 3px 0 0 red, -3px 0 0 blue;
+  }
+  76% {
+    text-shadow: 4px 0 0 red, -4px 0 0 blue;
+  }
+  77% {
+    text-shadow: 3px 0 0 red, -3px 0 0 blue;
+  }
+  78% {
+    text-shadow: 2px 0 0 red, -2px 0 0 blue;
+  }
+  79% {
+    text-shadow: 1px 0 0 red, -1px 0 0 blue;
+  }
+  80% {
+    text-shadow: 0px 0 0 red, 0px 0 0 blue;
+  }
+  81% {
+    text-shadow: 0.5px 0 0 red, -0.5px 0 0 lime;
+  }
+  82% {
+    text-shadow: 1px 0 0 red, -1px 0 0 lime;
+  }
+  83% {
+    text-shadow: 1.5px 0 0 red, -1.5px 0 0 lime;
+  }
+  84% {
+    text-shadow: 2px 0 0 red, -2px 0 0 lime;
+  }
+  85% {
+    text-shadow: 2.5px 0 0 red, -2.5px 0 0 lime;
+  }
+  86% {
+    text-shadow: 1.5px 0 0 red, -1.5px 0 0 lime;
+  }
+  87% {
+    text-shadow: 0.5px 0 0 red, -0.5px 0 0 lime;
+  }
+  88% {
+    text-shadow: -0.5px 0 0 red, 0.5px 0 0 lime;
+  }
+  89% {
+    text-shadow: -1.5px 0 0 red, 1.5px 0 0 lime;
+  }
+  90% {
+    text-shadow: -2.5px 0 0 red, 2.5px 0 0 lime;
+  }
+  91% {
+    text-shadow: 60px 0 0 lime, -60px 0 0 blue;
+  }
+  92% {
+    text-shadow: 0 0 0 lime, 0 0 0 blue;
+  }
+  92% {
+    text-shadow: 0.7px 0 0 blue, -0.7px 0 0 lime;
+  }
+  93% {
+    text-shadow: 1.4px 0 0 blue, -1.4px 0 0 lime;
+  }
+  94% {
+    text-shadow: 2.1px 0 0 blue, -2.1px 0 0 lime;
+  }
+  95% {
+    text-shadow: 2.8px 0 0 blue, -2.8px 0 0 lime;
+  }
+  96% {
+    text-shadow: 1.8px 0 0 blue, -1.8px 0 0 lime;
+  }
+  97% {
+    text-shadow: 0.8px 0 0 blue, -0.8px 0 0 lime;
+  }
+  98% {
+    text-shadow: -0.2px 0 0 blue, 0.2px 0 0 lime;
+  }
+  99% {
+    text-shadow: -1.2px 0 0 blue, 1.2px 0 0 lime;
+  }
+  100% {
+    text-shadow: -2.2px 0 0 blue, 2.2px 0 0 lime;
+  }
+}
+
+</style>
